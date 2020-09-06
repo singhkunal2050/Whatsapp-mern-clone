3dba071979ee8babba1b03ceb0e9b610883f1147
singhkunal2050 <singhkunal2050@gmail.com>
2020-09-06 21:13:02 +0530

    added the chat and sidebar component

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBCga2y6DnFz88-h-z1NyGmN1bIhT4fDgI",
+  authDomain: "whatsapp-mern-cf80a.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-cf80a.firebaseio.com",
+  projectId: "whatsapp-mern-cf80a",
+  storageBucket: "whatsapp-mern-cf80a.appspot.com",
+  messagingSenderId: "325646679034",
+  appId: "1:325646679034:web:2198f82379f65e3b550243"
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO REACT</h1>
+      {/* SIdebar component  */}
+      <Sidebar/>
+      {/* CHAt component */}
+      <Chat/>
+
+
     </div>
-  );
+
+);
 }
 
 export default App;
