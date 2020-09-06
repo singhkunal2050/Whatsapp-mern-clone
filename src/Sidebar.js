3260628e1f37b3548cdc19e9b51f53f983a6ab16
singhkunal2050 <singhkunal2050@gmail.com>
2020-09-06 22:05:18 +0530

    imported material icon package

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './Sidebar.css'
+import ChatIcon from '@material-ui/icons/Chat';
+
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
         <div className="sidebar__headerRight">
-
+          <ChatIcon />
         </div>
         <div className="sidebar__headerLeft">
-          
+
         </div>
       </div>
     </div>
