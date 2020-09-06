b789068244b15725d403f6137a7a36200534aa88
singhkunal2050 <singhkunal2050@gmail.com>
2020-09-06 22:32:53 +0530

    sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import './Sidebar.css'
 import ChatIcon from '@material-ui/icons/Chat';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
 
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <div className="sidebar__headerRight">
-          <ChatIcon />
-        </div>
         <div className="sidebar__headerLeft">
-
+          <IconButton>
+            <Avatar src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          </IconButton>
+        </div>
+        <div className="sidebar__headerRight  ">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
     </div>
