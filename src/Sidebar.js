25ea1c671360d72f7fbe0d686b563499b0e5ff9a
singhkunal2050 <singhkunal2050@gmail.com>
2020-09-06 21:55:20 +0530

    imported materail ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,14 @@ import './Sidebar.css'
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>      
+      <div className="sidebar__header">
+        <div className="sidebar__headerRight">
+
+        </div>
+        <div className="sidebar__headerLeft">
+          
+        </div>
+      </div>
     </div>
   )
 }
